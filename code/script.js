39564b1553a55621f444229626337ea9fa919f94
Bjorn <bjorn@example.com>
2026-05-04 08:59:27 +0200

    Step 1

--- code/script.js
+++ code/script.js
@@ -3,8 +3,15 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to our Javascript Pizzeria. The best pizzeria there is :)Ready to Start? - Click 'OK' to begin.`
 )
+// The great thing about the prompt method is that you can store the input in a variable. Your code goes here
+
+const name = prompt("Great! What's your name?");
+if (name === null || name.trim() === "") {
+  alert("No name given");
+} else {
+  alert(`Hi and welcome, ${name}!`);
 
 // Step 2 - Food choice
 // Your code goes here
